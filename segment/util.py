905fbff7f1f2ec7a2001617bd15e7c5bab9c3215
michael.liu <841412988@qq.com>
2018-05-15 17:46:02 +0800

    upload the seg relate code

--- segment/util.py
+++ segment/util.py
@@ -5,7 +5,9 @@ import time
 import codecs
 import pickle
 import numpy as np
+import logging
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 class Util(object):
     def __init__(self):
@@ -29,6 +31,7 @@ class Util(object):
         self.inputY = self.inputY[indexs]
 
     def initFile(self, inputPath, validPath=None, seqMaxLen=200):
+        logging.INFO("initFile starting.......")
         self.inputIndex = 0
         self.inputX, self.inputY = self.loadFile(inputPath, seqMaxLen)
 
@@ -68,11 +71,52 @@ class Util(object):
         Y = np.array(self.padding(Y, seqMaxLen))
         return X, Y
 
+    def bulidMap(self,trainPath):
+        charSet = set()
+        labelSet = set()
+        for line  in open(trainPath,'r', encoding='utf-8'):
+            line = line.strip()
+            if len(line)  == 0 : continue
+
+            terms = line.split()
+            if len(terms)  != 2:  continue
+            charSet.add(terms[0]) #char
+            labelSet.add(terms[1]) #label
+
+        chars = list(charSet)
+        labels = list(labelSet)
+
+        self.char2id = dict(zip(chars,range(1,len(chars) +1)))
+        self.label2id = dict(zip(labels,range(1,len(labels) + 1)))
+
+        self.id2char = dict(zip(range(1,len(chars) + 1),chars))
+        self.id2label = dict(zip(range(1,len(labels) + 1),labels))
+
+        self.id2char[0]  = "<PAD>"
+        self.id2label[0] = "<PAD>"
+        self.char2id["<PAD>"]  = 0
+        self.label2id["<PAD>"] = 0
+        self.id2char[len(chars) + 1] = "<NEW>"
+        self.char2id["<NEW>"] = len(chars) + 1
+
+        # save map
+        with open("char2id", "wb",encoding='utf-8') as outfile:
+            for idx in self.id2char:
+                outfile.write(self.id2char[idx] + "\t" + str(idx) + "\r\n")
+        with open("label2id", "wb",encoding='utf-8') as outfile:
+            for idx in self.id2label:
+                outfile.write(self.id2label[idx] + "\t" + str(idx) + "\r\n")
+        print( "saved map between token and id")
+
+    def loadMap(self, filePath):
+        if not os.path.isfile(filePath):
+            sys.exit("map file not exist")
+
 
 
 if __name__ == "__main__":
     util = Util()
-    util.loadFile( '../data/ChinaLangOrg.seg.label',200)
+    util.bulidMap( '../data/seg/ChinaLangOrg.seg.label')
     #print("kai x")
 
 
