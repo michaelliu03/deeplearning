8f7c325e58b66047ea664765ba2c977d1a0936ce
michael.liu <841412988@qq.com>
2018-06-27 23:00:22 +0800

    更新了相关的代码，添加了seq2seq model

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 深度学习的一些实践，而且这些实践主要用在自然语言处理方面。主要是通过对分词，命名实体的识别等的应用场景来学习
-TensorFlow 以及深度学习
+TensorFlow 以及深度学习 
+
+Project 1： 中文分词 
+
+Project 2： seq2seq Model
+
+
 
 感谢牛人的支持
 segment:
---
+++ seq2seq/preprocessing.py
@@ -0,0 +1,15 @@
+import jieba
+
+class Processor():
+    __PAD__ = 0
+    __GO__ = 1
+    __EOS__ = 2
+    __UNK__ = 3
+    vocab = ['__PAD__', '__GO__', '__EOS__', '__UNK__']
+
+
+
+
+
+
+preprocess = Processor()
